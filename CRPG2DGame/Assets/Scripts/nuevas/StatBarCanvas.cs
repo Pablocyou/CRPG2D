2b3d9330a7cf6844e4c83937708b645726fd471b
AndreuRaga <andreurag@gmail.com>
2024-03-29 17:34:43 +0100

    Cambio de nombre de script
    HealthBar -> StatBarCanvas

--- CRPG2DGame/Assets/Scripts/nuevas/StatBarCanvas.cs
+++ CRPG2DGame/Assets/Scripts/nuevas/StatBarCanvas.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class StatBarCanvas : MonoBehaviour
 {
     public StatPoints hitPoints;
     public Image meterImage;
